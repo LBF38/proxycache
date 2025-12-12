dad5620f9009bcab99509ea8d0d4e91104286e41
LBF38 <contact.lbf38@gmail.com>
2025-12-12 10:24:55 +0100

    feat(proxy): implement x-forwarded-for
    add tests + check errors

--- internal/proxy.go
+++ internal/proxy.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"net/url"
 )
@@ -21,7 +22,22 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	r.URL.Host = origin.Host
 	r.URL.Scheme = origin.Scheme
 	r.RequestURI = ""
+	if r.RemoteAddr != "" {
+		h, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("error remote addr %v", err)
+			return
+		}
+		r.Header.Set("X-Forwarded-For", h)
+	}
+
 	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("error requesting server: %v", err)
+		return
+	}
 
 	for key, values := range resp.Header {
 		for _, value := range values {
--- internal/proxy_test.go
+++ internal/proxy_test.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -28,5 +29,23 @@ func TestProxy(t *testing.T) {
 		require.Equal(t, "text/plain", response.Header().Get("content-type"))
 		require.Equal(t, "some value", response.Header().Get("X-testing-header"))
 		require.Equal(t, "some test", response.Body.String())
+	})
+	t.Run("X-Forwarded-For", func(t *testing.T) {
+		var header string
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			header = r.Header.Get("X-Forwarded-For")
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintf(w, "body content")
+		}))
+		defer server.Close()
+		proxy := &Proxy{server.URL}
+		req, _ := http.NewRequest(http.MethodGet, server.URL, nil)
+		req.RemoteAddr = "10.0.0.1:45"
+		response := httptest.NewRecorder()
+
+		proxy.ServeHTTP(response, req)
+
+		require.Equal(t, "10.0.0.1", header)
+		log.Println(response.Body.String())
 	})
 }
