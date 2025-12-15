10acf84b2d62e350d1c18ab0e906361ed34da6a3
LBF38 <contact.lbf38@gmail.com>
2025-12-15 11:33:20 +0100

    feat(cache): in-memory cache
    add minimal implementation for using cache in the main program

---
+++ internal/in_memory_cache.go
@@ -0,0 +1,28 @@
+package internal
+
+import "sync"
+
+type InMemoryCache struct {
+	store map[string]*CacheEntity
+	mu    sync.RWMutex
+}
+
+func NewInMemoryCache(size int) *InMemoryCache {
+	c := new(InMemoryCache)
+	c.store = make(map[string]*CacheEntity, size)
+	return c
+}
+
+func (c *InMemoryCache) Get(key string) (*CacheEntity, error) {
+	c.mu.RLock()
+	defer c.mu.RLocker().Unlock()
+	val := c.store[key]
+	return val, nil
+}
+
+func (c *InMemoryCache) Set(key string, value *CacheEntity) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.store[key] = value
+	return nil
+}
